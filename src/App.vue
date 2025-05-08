becca1e46d572c93c7bc8ac9e4c7deb34a220dcb
CanonNi <canonnizq@gmail.com>
2025-05-08 10:36:42 +0800

    feat(App): add tk font & ss

--- src/App.vue
+++ src/App.vue
@@ -9,6 +9,10 @@ import Socials from './components/Socials.vue'
   <main>
     <Matrix />
     <div class="center">
+      <div class="logos">
+        <img src="/img/lipu.svg" />
+        <img src="/img/kamapona.svg" />
+      </div>
       <Typewriter />
       <Subdomains />
       <Socials />
@@ -28,5 +32,14 @@ main {
 .center {
   display: flex;
   flex-direction: column;
+}
+
+.logos {
+  height: 10rem;
+  display: flex;
+  opacity: 0.65;
+  justify-content: center;
+  margin-bottom: -5rem;
+  margin-top: 5rem;
 }
 </style>
