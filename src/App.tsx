8f6fa0ffeb2e492b43cc5c1b702cf5f4c875963b
CanonNi <canonnizq@gmail.com>
2025-12-17 23:20:21 +0800

    fix: general fixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
 import './styles/App.css';
 
-import { Socials, Subdomains, Typewriter } from './components';
+import { Socials, Typewriter } from './components';
 
 export default function App() {
   return (
     <main>
       <div className="center">
-        {/* <Subdomains /> */}
         <Typewriter />
         <Socials />
       </div>
--- src/components/Typewriter.tsx
+++ src/components/Typewriter.tsx
@@ -8,79 +8,77 @@ export default function Typewriter() {
 
   const typingSpeed = 100;
   const [displayedText, setDisplayedText] = useState('');
-  if (!isMobile) {
-    const fullTexts = [
-      'CanonNi',
-      'canonnizq',
-      'Ni Zeqing',
-      '倪泽青',
-      'CNZQ',
-      'Pongo sapiens',
-      'Kinney',
-      'BI4APQ',
-    ];
+  const fullTexts = [
+    'CanonNi',
+    'canonnizq',
+    'Ni Zeqing',
+    '倪泽青',
+    'CNZQ',
+    'Pongo sapiens',
+    'Kinney',
+    'BI4APQ',
+  ];
 
-    const shuffle = (array: string[]) => {
-      for (let i = array.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [array[i], array[j]] = [array[j], array[i]];
+  const shuffle = (array: string[]) => {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+  };
+
+  useEffect(() => {
+    let letter_idx = 0;
+    let currentText = '';
+    let order = shuffle([...fullTexts]);
+    let timeoutId: number;
+
+    const type = () => {
+      if (letter_idx == 0) {
+        currentText = order.pop()!;
+        if (order.length == 0) {
+          order = shuffle([...fullTexts]);
+        }
+        setDisplayedText('');
       }
-      return array;
+
+      if (letter_idx < currentText.length) {
+        setDisplayedText((prev) => prev + currentText[letter_idx - 1]);
+        letter_idx++;
+        timeoutId = setTimeout(type, typingSpeed);
+      } else {
+        letter_idx = 0;
+        timeoutId = setTimeout(type, typingSpeed * 10);
+      }
     };
 
-    useEffect(() => {
-      let letter_idx = 0;
-      let currentText = '';
-      let order = shuffle([...fullTexts]);
-      let timeoutId: number;
+    type();
 
-      const type = () => {
-        if (letter_idx == 0) {
-          currentText = order.pop()!;
-          if (order.length == 0) {
-            order = shuffle([...fullTexts]);
-          }
-          setDisplayedText('');
-        }
+    return () => clearTimeout(timeoutId);
+  }, []);
 
-        if (letter_idx < currentText.length) {
-          setDisplayedText((prev) => prev + currentText[letter_idx - 1]);
-          letter_idx++;
-          timeoutId = setTimeout(type, typingSpeed);
-        } else {
-          letter_idx = 0;
-          timeoutId = setTimeout(type, typingSpeed * 10);
-        }
-      };
-
-      type();
-
-      return () => clearTimeout(timeoutId);
-    }, [isMobile]);
-
-    return (
-      <div className={styles.container}>
-        <span className={`${styles.text} monospace`}>
-          {isMobile ? 'CanonNi' : displayedText}
-          {isMobile ? null : <span className={styles.cursor}>|</span>}
-        </span>
-        <div className={styles.domain}>
-          <span className={styles.tooltip}>Yup, that's an actual TLD.</span>
-          <span className={styles.middle}>
-            <span className={styles.tld}>.website</span>
-            <span className={styles.links}>
-              <a target="_blank" href="https://github.com/canonnizq/canonni.website">
-                source
-              </a>
-              &bull;
-              <a target="_blank" href="https://afdian.com/a/canonni">
-                sponsor
-              </a>
-            </span>
+  return (
+    <div className={styles.container}>
+      <span className={`${styles.text} monospace`}>
+        {isMobile ? 'CanonNi' : displayedText}
+        {isMobile ? null : <span className={styles.cursor}>|</span>}
+      </span>
+      <div className={styles.domain}>
+        <span className={styles.tooltip}>Yup, that's an actual TLD.</span>
+        <span className={styles.middle}>
+          <span className={styles.tld}>.website</span>
+          <span className={styles.links}>
+            <a target="_blank" href="https://github.com/canonnizq/canonni.website">
+              source
+            </a>
+            &bull;
+            <a target="_blank" href="https://afdian.com/a/canonni">
+              sponsor
+            </a>
           </span>
-          <span className={styles.tooltip}>Pretty apt for a website, huh?</span>
-        </div>
+        </span>
+        <span className={styles.tooltip}>Pretty apt for a website, huh?</span>
       </div>
-    );
-  }
+    </div>
+  );
 }
